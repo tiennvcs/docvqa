613e3198a05934fa251b3f4dd046ae30ba7b2282
Tien Nguyen <57661349+tiennvcs@users.noreply.github.com>
2021-09-13 10:27:00 +0700

    Create install.sh

---
+++ libs/layoutlmv2/install.sh
@@ -0,0 +1,18 @@
+pip install transformers
+
+pip install pyyaml==5.1
+
+
+pip install torch==1.8.0+cu101 torchvision==0.9.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html
+
+
+pip install detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/torch1.8/index.html
+
+
+pip install datasets
+
+
+apt install tesseract-ocr
+
+
+pip install pytesseract
